056f993626fc1e7e6cd74d76111b073d2bae27ee
Laka Nodia <laka.nodia@gmail.com>
2022-11-30 23:03:02 +0400

    Add auth module and install angular materials

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    RegistrationComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }

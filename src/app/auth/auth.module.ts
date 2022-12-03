b703fec18e020c1846ea8f4732a54d9ba1eab52a
Laka Nodia <laka.nodia@gmail.com>
2022-12-03 18:02:37 +0400

    Add forms

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,18 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
-
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    RegistrationComponent,
-    LoginComponent
-  ],
+  declarations: [RegistrationComponent, LoginComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatTabsModule,
+  ],
+  exports: [RegistrationComponent, LoginComponent],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  hide: any;
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+  get email() {
+    return this.loginForm.get('email') as FormControl;
   }
+  get password() {
+    return this.loginForm.get('password') as FormControl;
+  }
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }

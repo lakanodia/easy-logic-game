9c69fa34fb08d99710fa0d5a54a1c33bc8e602e4
Laka Nodia <laka.nodia@gmail.com>
2022-12-03 14:12:20 +0400

    Install materials

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+
+// const heroesRoutes: Routes = [
+//   { path: 'heroes', redirectTo: '/superheroes' },
+//   { path: 'hero/:id', redirectTo: '/superhero/:id' },
+//   { path: 'superheroes',  component: HeroListComponent, data: { animation: 'heroes' } },
+//   { path: 'superhero/:id', component: HeroDetailComponent, data: { animation: 'hero' } }
+// ];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class AuthRoutingModule {}
